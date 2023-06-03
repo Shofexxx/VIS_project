2c72461c1a97d358e493e21faa947e9205a151e8
Shofexxx <97199086+Shofexxx@users.noreply.github.com>
2023-06-03 20:44:27 +0200

    -default construct

---
+++ backend/src/test/java/cz/vsb/ekf/haj0185/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.vsb.ekf.haj0185.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
